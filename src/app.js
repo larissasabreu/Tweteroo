090e78f3216f4792a3005fb8290e377f097bc18d
Lari <larissaabreux@gmail.com>
2024-06-23 03:22:36 -0300

    validaçao

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import { MongoClient } from 'mongodb';
 import Joi from 'joi';
+import cors from 'cors';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -18,24 +19,58 @@ mongoClient.connect()
     })
     .catch((err) => console.log(err.message))
 
-// validação 
-// const userSchema = joi.object({
-//     username: joi.string().required();
-//     avatar: joi.string().email().required();
-//   });
+// Logon
+app.post("/sign-up", (req, res) => {
+    const user = req.body;
+    // validação
+    const userSchema = Joi.object({
+    username: Joi.string().required(),
+    avatar: Joi.string().required()
+    });
 
-// const tweetSchema = joi.object({
-//     username: joi.string().required();
-//     avatar: joi.string().required();
-// })
+    const validation = userSchema.validate(user, {abortEarly: false});
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
 
-app.post("/tweteroo", (req, res) => {
+    // post
+    db.collection("users").insertOne({
+        user
+    }).then(users => res.sendStatus(201))
+    .catch(err => console.log(err.message))
+})
+
+app.get("/sign-up", (req, res) => {
+    db.collection("users").find().toArray()
+    .then(data => res.send(data))
+    .catch(err => res.status(500).send(err.message))
+})
+
+// Tweets
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    
+    // validação
+    const tweetSchema = Joi.object({
+    username: Joi.string().required(),
+    tweet: Joi.string().required()
+    })
+
+    const validation = tweetSchema.validate(tweet, {abortEarly: false});
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    // post
     db.collection("tweets").insertOne({
+        tweet
     }).then(tweets => res.sendStatus(201))
       .catch(err => console.log(err.message))
 })
 
-app.get("/tweteroo", (req, res) => {
+app.get("/tweets", (req, res) => {
     db.collection("tweets").find().toArray()
         .then(data => res.send(data))
         .catch(err => res.status(500).send(err.message))
