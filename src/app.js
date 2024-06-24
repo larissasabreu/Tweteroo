56a3e14eb15f5f027d912374c3eb950dbf1c4cf4
Lari <larissaabreux@gmail.com>
2024-06-24 14:49:57 -0300

    edit e delete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express, { json } from 'express';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import Joi from 'joi';
 import cors from 'cors';
 import dotenv from "dotenv";
@@ -7,11 +7,12 @@ dotenv.config();
 
 const app = express();
 app.use(json());
+app.use(cors());
+
 
 // mongoDb setup
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
-
 mongoClient.connect()
     .then(() => {
         console.log("teste");
@@ -19,15 +20,22 @@ mongoClient.connect()
     })
     .catch((err) => console.log(err.message))
 
+
+// schema para a validação dos tweets
+const tweetSchema = Joi.object({
+    username: Joi.string().required(),
+    tweet: Joi.string().required()
+    })
+
 // Logon
 app.post("/sign-up", (req, res) => {
     const user = req.body;
+
     // validação
     const userSchema = Joi.object({
     username: Joi.string().required(),
     avatar: Joi.string().required()
     });
-
     const validation = userSchema.validate(user, {abortEarly: false});
     if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message);
@@ -35,46 +43,95 @@ app.post("/sign-up", (req, res) => {
     }
 
     // post
-    db.collection("users").insertOne({
+    db.collection("users").insertOne(
         user
-    }).then(users => res.sendStatus(201))
+    ).then(res.sendStatus(201))
     .catch(err => console.log(err.message))
 })
 
+
+// get usuário signupado
 app.get("/sign-up", (req, res) => {
-    db.collection("users").find().toArray()
-    .then(data => res.send(data))
-    .catch(err => res.status(500).send(err.message))
+    const { id } = req.params;
+  
+    db.collection("users").findOne({ id })
+          .then((data) => {
+              return res.send(data);
+          })
+          .catch(() => {
+              return res.status(500).send(err)
+          })
 })
 
-// Tweets
+// post Tweets
 app.post("/tweets", (req, res) => {
     const tweet = req.body;
-    
-    // validação
-    const tweetSchema = Joi.object({
-    username: Joi.string().required(),
-    tweet: Joi.string().required()
-    })
 
+    // validação
+    const validation = tweetSchema.validate(tweet, {abortEarly: false});
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    } else if (!tweet.username) {
+        return res.sendStatus(401)
+    }
+
+    // post
+    db.collection("tweets").insertOne(
+        tweet
+    ).then(res.sendStatus(201))
+      .catch(err => console.log(err.message))
+})
+
+
+
+// get tweets
+app.get("/tweets", async (req, res) => {
+
+    try {
+    const getter = await db.collection("tweets").find().toArray()
+    
+    res.send(getter)
+
+    } catch (err) {
+        return res.send(err)
+    }
+})
+
+// edit
+app.put("/tweets/:id", async (req, res) => {
+    const { id } = req.params;
+    const tweet = req.body;
+
+    // validação
     const validation = tweetSchema.validate(tweet, {abortEarly: false});
     if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message);
         return res.status(422).send(errors);
     }
 
-    // post
-    db.collection("tweets").insertOne({
-        tweet
-    }).then(tweets => res.sendStatus(201))
-      .catch(err => console.log(err.message))
-})
+    try {
+        const result = await db.collection("tweets").updateOne({ _id: new ObjectId(id) },
+        {$set: tweet});
+        if (result.matchedCount === 0) return res.sendStatus(404);
+		res.status(204).send("Tweet editado com sucesso!");
+    } catch (error) {
+        res.status(404).send(error);
+       }
+}) 
 
-app.get("/tweets", (req, res) => {
-    db.collection("tweets").find().toArray()
-        .then(data => res.send(data))
-        .catch(err => res.status(500).send(err.message))
-})
+// delete
+app.delete("/tweets/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+		const result = await db.collection("tweets").deleteOne({ _id: new ObjectId(id) });
+		if (result.deletedCount === 0) return res.sendStatus(404);
+		res.status(204).send("Tweet deletado com sucesso!");
+	} catch (error) {
+	  res.status(404).send(error);
+	}
+  })
 
 
 const port = process.env.PORT
